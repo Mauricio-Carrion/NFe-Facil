31eb9e0e7bdb9d56bfbb5234088a1a443877b36a
Mauricio-Carrion <mauricio.sjn@gmail.com>
2022-08-10 21:58:55 -0300

    Adicionando JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,21 @@
 // Scroll topo
-window.onscroll = function () { headerFixed() };
+window.onscroll = function () { headerFixed(), showBackToTop() };
 
 function headerFixed() {
-  if (document.documentElement.scrollTop > 45) {
-    document.getElementById('topo').className = 'fixed-top'
-    document.querySelector('body').style.paddingTop = '45px'
+  if (document.documentElement.scrollTop > 32) {
+    document.getElementById('header').className = 'fixed-top'
+    document.querySelector('body').style.paddingTop = '32px'
   }
   else {
-    document.getElementById('topo').className = ''
+    document.getElementById('header').className = ''
     document.querySelector('body').style.paddingTop = ''
   }
 };
-
+//Color Span DropDown
 document.getElementById('ulDownloads').onmouseover = function () { mouseoverUlDownloads() }
 document.getElementById('ulDownloads').onmouseout = function () { mouseoutUlDownloads() }
 document.getElementById('ulRestrita').onmouseover = function () { mouseoverUlRestrita() }
 document.getElementById('ulRestrita').onmouseout = function () { mouseoutUlRestrita() }
-
 
 function mouseoverUlDownloads() {
   document.getElementById('downloads').style.color = '#0087c9'
@@ -33,6 +32,34 @@ function mouseoverUlRestrita() {
 function mouseoutUlRestrita() {
   document.getElementById('restrita').style.color = ''
 };
+
+// Mostrar botão voltar ao topo
+function showBackToTop() {
+  if (document.documentElement.scrollTop > 550) {
+    document.getElementById('backToTop').style.display = 'block'
+  } else {
+    document.getElementById('backToTop').style.display = 'none'
+  }
+};
+
+//Efeito onMouseOver BackToTop
+document.getElementById('backToTop').onmouseover = function () { backToTopOver() }
+document.getElementById('backToTop').onmouseout = function () { backToTopOut() }
+
+function backToTopOver() {
+  document.getElementById('backToTop').style.marginRight = '-35px'
+}
+
+function backToTopOut() {
+  document.getElementById('backToTop').style.marginRight = '-45px'
+};
+
+// ScrollReveal
+ScrollReveal().reveal('.headline')
+ScrollReveal().reveal('.tagline', { delay: 450 })
+ScrollReveal().reveal('.punchline-card1', { delay: 750 })
+ScrollReveal().reveal('.punchline-card2', { delay: 950 })
+ScrollReveal().reveal('.punchline-card3', { delay: 1150 });
 
 
 
